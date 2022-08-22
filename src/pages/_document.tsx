3b2d132ede75a9b6a86147fdd186da81d9096297
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-08-22 02:27:38 +0000

    instalação MUI integrado com renderização no servidor

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import createEmotionCache from "../data/services/EmotionCache";
+import createEmotionServer from "@emotion/server/create-instance";
 
 export default class EdiaristasDocument extends Document {
   render(): JSX.Element {
@@ -10,6 +18,17 @@ export default class EdiaristasDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet"
+          />
+          {(this.props as any).emotionStyleTags}
         </Head>
         <body>
           <Main />
@@ -19,3 +38,35 @@ export default class EdiaristasDocument extends Document {
     );
   }
 }
+
+EdiaristasDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage;
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App: any) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+
+  const emotionStyleTags = emotionStyles.styles.map((style) => {
+    return (
+      <style
+        data-emotion={`${style.key} ${style.ids.join(" ")}`}
+        key={style.key}
+        dangerouslySetInnerHTML={{ __html: style.css }}
+      />
+    );
+  });
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+  };
+};
