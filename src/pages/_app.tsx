df5712e975232648bd9aaf04e822545a16d2545f
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-08-22 01:27:12 +0000

    preparo podermos farzer a inclusão do MUI nas páginas estáticas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import "../ui/styles/globals.css";
+import "@styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>e-diaristas</title>
+
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../ui/styles/Home.module.css";
+import styles from "@styles/Home.module.css";
+import { Button } from "@mui/material";
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <Button variant="contained">Clique-me!</Button>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
