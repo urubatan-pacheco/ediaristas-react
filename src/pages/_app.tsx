baa204bf77a57cf0454b31b3fae468a3c0850195
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-08-24 02:25:50 +0000

    instalação MUI integrado com renderização no servidor - remoção de css do servidor desnecessário no cliente

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
+import { useEffect } from "react";
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    document.querySelector("#jss-server-side")?.remove();
+  }, []);
+
   return (
     <>
       <Head>
         <title>e-diaristas</title>
-
       </Head>
       <Component {...pageProps} />
     </>
