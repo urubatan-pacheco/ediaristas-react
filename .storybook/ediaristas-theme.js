6188015afacbf4352a44a5854164f171fca97c46
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-08-29 23:45:34 +0000

    Configurando o Storybook

---
+++ .storybook/ediaristas-theme.js
@@ -0,0 +1,7 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  brandTitle: "e-diaristas",
+  brandImage: "./img/logos/logo.svg",
+  brandUrl: "http://192.168.234.128:3000/",
+});
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import "../src/ui/styles/globals.css";
+import StoryThemeProvider from "../src/ui/themes/StoryThemeProvider";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +9,6 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [StoryThemeProvider];
