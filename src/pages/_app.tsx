073d6c8f6c60579875c09cc92d374a91ded025f8
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-09-02 21:47:15 +0000

    Arrumando o layout da página

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,9 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "@mui/material";
 import theme from "ui/themes/theme";
+import Header from "ui/components/surfaces/Header/Header";
+import Footer from "ui/components/surfaces/Footer/Footer";
+import { AppContainer } from "@styles/pages/_app.styled";
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -13,10 +16,16 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>e-diaristas</title>
+        <title>e-diaristas {pageProps.title && ` -  ${pageProps.title}`}</title>
       </Head>
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <AppContainer>
+          <Header />
+          <main>
+            <Component {...pageProps} />
+          </main>
+          <Footer />
+        </AppContainer>
       </ThemeProvider>
     </>
   );
