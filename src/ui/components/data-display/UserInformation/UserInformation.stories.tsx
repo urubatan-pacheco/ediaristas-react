6144c576a3fca27ede8e96cc1902d3946303d3f6
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-09-28 02:35:15 +0000

    Buscar diaristas por CEP - Componente de exibição de Usuários

---
+++ src/ui/components/data-display/UserInformation/UserInformation.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import UserInformation from './UserInformation';
+
+export default {
+  title: 'data-display/UserInformation',
+  comppnent: UserInformation,
+} as ComponentMeta<typeof UserInformation>;
+
+const Template: ComponentStory<typeof UserInformation> = (args) => (
+  <UserInformation {...args} />
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  name: 'Urubatan Pacheco',
+  picture: 'https://github.com/urubatan-pacheco.png',
+  rating: 5,
+  description: 'Web developer',
+  isRating: true,
+};
