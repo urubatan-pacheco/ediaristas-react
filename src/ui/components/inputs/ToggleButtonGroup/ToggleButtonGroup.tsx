aaac9e95b78388ced59e0339bb6cf031c684b77e
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-10-06 21:33:30 +0000

    Criando componentes - Grupo de botões

---
+++ src/ui/components/inputs/ToggleButtonGroup/ToggleButtonGroup.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import {} from '@mui/material';
+import {
+    ToggleButtonGroupStyled,
+    ToggleButtonStyled,
+} from './ToggleButtonGroup.style';
+
+export interface ToggleButtonGroupProps {}
+
+const ToggleButtonGroup = ToggleButtonGroupStyled;
+export const ToggleButton = ToggleButtonStyled;
+
+export default ToggleButtonGroup;
