ea5d5dbcb290d4b1856818f6309852baebe742ef
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-10-07 01:46:02 +0000

    Criando componentes - Select

---
+++ src/ui/components/inputs/Select/Select.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import {
+    FormControl,
+    InputLabel,
+    SelectProps as MuiSelectProps,
+} from '@mui/material';
+import { SelectStyled } from './Select.style';
+import { v4 as uuid } from 'uuid';
+
+export interface SelectProps extends MuiSelectProps {
+    label?: string;
+}
+
+const Select: React.FC<SelectProps> = ({
+    label,
+    children,
+    style,
+    ...props
+}) => {
+    const [elementId, setElementId] = useState('');
+    useEffect(() => {
+        if (window !== undefined) {
+            setElementId(uuid());
+        }
+    }, []);
+
+    return (
+        <FormControl variant={'outlined'} style={style}>
+            <InputLabel id={elementId}>{label}</InputLabel>
+            <SelectStyled labelId={elementId} label={label} {...props}>
+                {children}
+            </SelectStyled>
+        </FormControl>
+    );
+};
+
+export default Select;
