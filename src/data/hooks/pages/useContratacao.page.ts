1ded645987606080f0a1cf571b0b14ec6ed795f0
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-12-06 22:53:31 -0300

    React Context APIs e Serviços Listagem do serviços disponíveis

--- src/data/hooks/pages/useContratacao.page.ts
+++ src/data/hooks/pages/useContratacao.page.ts
@@ -9,9 +9,10 @@ import {
     PagamentoFormDataInterface,
 } from 'data/@types/FormInterface';
 import { ServicoInterface } from 'data/@types/ServicoInterface';
+import useApi from '../useApi.hook';
 
 export default function useContratacao() {
-    const [step, setStep] = useState(4),
+    const [step, setStep] = useState(1),
         [hasLogin, setHasLogin] = useState(false),
         [loginError, setLoginError] = useState(''),
         breadCrumbItems = ['Detalhes da diária', 'Identificação', 'Pagamento'],
@@ -35,71 +36,7 @@ export default function useContratacao() {
         loginForm = useForm<LoginFormDataInterface>({
             resolver: yupResolver(FormSchemaService.login()),
         }),
-        servicos: ServicoInterface[] = [
-            {
-                id: 2,
-                nome: 'Limpeza convencional',
-                valor_minimo: 100.0,
-                qtd_horas: 1,
-                porcentagem_comissao: 10.0,
-                horas_quarto: 1,
-                valor_quarto: 20.0,
-                horas_sala: 1,
-                valor_sala: 20.0,
-                horas_banheiro: 1,
-                valor_banheiro: 20.0,
-                horas_cozinha: 1,
-                valor_cozinha: 20.0,
-                horas_quintal: 1,
-                valor_quintal: 20.0,
-                horas_outros: 1,
-                valor_outros: 20.0,
-                icone: 'upf-cleaning-1',
-                posicao: 1,
-            },
-            {
-                id: 3,
-                nome: 'Limpeza pesada',
-                valor_minimo: 120.0,
-                qtd_horas: 1.2,
-                porcentagem_comissao: 10.0,
-                horas_quarto: 1.2,
-                valor_quarto: 30.0,
-                horas_sala: 1.2,
-                valor_sala: 30.0,
-                horas_banheiro: 1.2,
-                valor_banheiro: 30.0,
-                horas_cozinha: 1.2,
-                valor_cozinha: 30.0,
-                horas_quintal: 1.2,
-                valor_quintal: 30.0,
-                horas_outros: 1.2,
-                valor_outros: 30.0,
-                icone: 'upf-cleaning-2',
-                posicao: 2,
-            },
-            {
-                id: 4,
-                nome: 'Limpeza pós obra',
-                valor_minimo: 150.0,
-                qtd_horas: 1.5,
-                porcentagem_comissao: 10.0,
-                horas_quarto: 1.5,
-                valor_quarto: 50.0,
-                horas_sala: 1.5,
-                valor_sala: 50.0,
-                horas_banheiro: 1.5,
-                valor_banheiro: 50.0,
-                horas_cozinha: 1.5,
-                valor_cozinha: 50.0,
-                horas_quintal: 1.5,
-                valor_quintal: 50.0,
-                horas_outros: 1.5,
-                valor_outros: 50.0,
-                icone: 'upf-cleaning-3',
-                posicao: 3,
-            },
-        ];
+        servicos = useApi<ServicoInterface[]>('/api/servicos').data;
 
     function onServiceFormSubmit(data: NovaDiariaFormDataInterface) {
         console.log(data);
--- src/ui/partials/encontrar-diarista/_contratacao.tsx
+++ src/ui/partials/encontrar-diarista/_contratacao.tsx
@@ -1,4 +1,10 @@
-import { Box, Button, Paper, Typography } from '@mui/material';
+import {
+    Box,
+    Button,
+    CircularProgress,
+    Paper,
+    Typography,
+} from '@mui/material';
 import useContratacao from 'data/hooks/pages/useContratacao.page';
 import useIsMobile from 'data/hooks/useIsMobile';
 import React, { PropsWithChildren } from 'react';
@@ -13,6 +19,7 @@ import DetalhesServico from './_detalhes-servico';
 import CadastroCliente, { LoginCliente } from './_cadastro-cliente';
 import InformacoesPagamento from './_informacoes-pagamento';
 import Link from 'ui/components/navigation/Link/Link';
+import { Container } from '@mui/system';
 
 // import { Component } from './_contratacao.styled';
 
@@ -35,6 +42,15 @@ const Contratacao: React.FC<PropsWithChildren> = () => {
             setHasLogin,
             loginError,
         } = useContratacao();
+
+    if (!servicos || servicos.length < 1) {
+        return (
+            <Container sx={{ textAlign: 'center', my: 10 }}>
+                <CircularProgress />
+            </Container>
+        );
+    }
+
     return (
         <div>
             {!isMobile && <SafeEnvironment />}
