4abafc66b6495c72806f97c8caf9d6365b609e5c
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2022-12-17 23:49:33 -0300

    Integração com Back-End/Autenticação e Roteamento Tratatamento de requisições

--- src/data/services/ApiService.ts
+++ src/data/services/ApiService.ts
@@ -1,5 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { ApiLinksInterface } from 'data/@types/ApiLinksInterface';
+import { on } from 'events';
+import { LocalStorage } from './StorageService';
 
 const url = process.env.NEXT_PUBLIC_API;
 
@@ -9,6 +11,50 @@ export const ApiService = axios.create({
         'Content-Type': 'application/json',
     },
 });
+
+ApiService.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (
+            error.response.status === 401 &&
+            error.response.data.code === 'token_not_valid'
+        ) {
+            handleTokenRefresh(error);
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+async function handleTokenRefresh(error: { config: AxiosRequestConfig }) {
+    const tokenRefresh = LocalStorage.get<string>('token_refresh', '');
+    if (tokenRefresh) {
+        // verifica se já fizemos login
+        LocalStorage.clear('token_refresh');
+        LocalStorage.clear('token');
+        try {
+            const { data } = await ApiService.post('/auth/token/refresh/', {
+                refresh: tokenRefresh,
+            });
+            LocalStorage.set('token', data.access);
+            LocalStorage.set('token_refresh', data.refresh);
+
+            ApiService.defaults.headers.common.Authorization =
+                'Bearer ${data.access}';
+
+            // precisamos refazer a requisição
+            error.config.headers!.Authorization =
+                ApiService.defaults.headers.common.Authorization;
+
+            return ApiService(error.config);
+        } catch (err) {
+            // neste caso o token-refresh não estava mais valido
+            return error;
+        }
+    } else {
+        return error;
+    }
+}
 
 export function linksResolver(
     links: ApiLinksInterface[] = [],
