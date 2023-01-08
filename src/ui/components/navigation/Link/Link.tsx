59e07a9b69ac94e787aec11f704dd106344e43f2
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2023-01-07 22:40:44 -0300

    Correção com inclusão em Props do onClick

--- src/ui/components/navigation/Link/Link.tsx
+++ src/ui/components/navigation/Link/Link.tsx
@@ -3,39 +3,40 @@ import { PropsWithChildren } from 'react';
 import NextLink, { LinkProps as NextLinkProps } from 'next/link'; // Link é export default podemos renomear de cara
 import Router from 'next/router';
 import {
-  Link as MuiLink,
-  LinkProps as MuiLinkProps,
-  ButtonProps,
+    Link as MuiLink,
+    LinkProps as MuiLinkProps,
+    ButtonProps,
 } from '@mui/material';
 
 export interface LinkProps {
-  href: string;
-  mui?: MuiLinkProps | ButtonProps;
-  next?: NextLinkProps;
-  Component?: React.ElementType;
+    href: string;
+    mui?: MuiLinkProps | ButtonProps;
+    next?: NextLinkProps;
+    Component?: React.ElementType;
+    onClick?: () => void;
 }
 
 const Link: React.FC<PropsWithChildren<LinkProps>> = ({
-  children,
-  href,
-  mui,
-  next,
-  Component = MuiLink,
-  ...props
+    children,
+    href,
+    mui,
+    next,
+    Component = MuiLink,
+    ...props
 }) => {
-  const isNextEnv = Boolean(Router.router);
+    const isNextEnv = Boolean(Router.router);
 
-  return isNextEnv ? (
-    <NextLink href={href} passHref {...next}>
-      <Component {...mui} {...props}>
-        {children}
-      </Component>
-    </NextLink>
-  ) : (
-    <Component href={href} {...mui} {...props}>
-      {children}
-    </Component>
-  );
+    return isNextEnv ? (
+        <NextLink href={href} passHref {...next}>
+            <Component {...mui} {...props}>
+                {children}
+            </Component>
+        </NextLink>
+    ) : (
+        <Component href={href} {...mui} {...props}>
+            {children}
+        </Component>
+    );
 };
 
 export default Link;
