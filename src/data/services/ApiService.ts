5215cb0e54ebfde8415d7c51b71211f885c99b95
Urubatan R. Pacheco <urubatan.pacheco@gmail.com>
2023-01-28 01:21:31 -0300

    correções sugeridas pelo backend para problemas no  JWT

--- src/data/services/ApiService.ts
+++ src/data/services/ApiService.ts
@@ -1,6 +1,5 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { ApiLinksInterface } from 'data/@types/ApiLinksInterface';
-import { on } from 'events';
 import { LocalStorage } from './StorageService';
 
 const url = process.env.NEXT_PUBLIC_API;
@@ -15,11 +14,12 @@ export const ApiService = axios.create({
 ApiService.interceptors.response.use(
     (response) => response,
     (error) => {
+        console.log('#### interceptor.response.use', error);
         if (
-            error.response.status === 401 &&
+            error?.response?.status === 401 &&
             error.response.data.code === 'token_not_valid'
         ) {
-            handleTokenRefresh(error);
+            return handleTokenRefresh(error);
         }
 
         return Promise.reject(error);
@@ -28,15 +28,23 @@ ApiService.interceptors.response.use(
 
 async function handleTokenRefresh(error: { config: AxiosRequestConfig }) {
     const tokenRefresh = LocalStorage.get<string>('token_refresh', '');
+    console.log(
+        '%%%%% handleTokenRefresh tokenrefresh:',
+        tokenRefresh,
+        'error:',
+        error
+    );
     if (tokenRefresh) {
         // verifica se já fizemos login
         LocalStorage.clear('token_refresh');
         LocalStorage.clear('token');
+        ApiService.defaults.headers.common.Authorization = '';
+        error.config.headers!.Authorization = '';
         try {
             const { data } = await ApiService.post<{
                 access: string;
                 refresh: string;
-            }>('/auth/token/refresh/', {
+            }>('/auth/refresh/', {
                 refresh: tokenRefresh,
             });
             LocalStorage.set('token', data.access);
@@ -45,23 +53,24 @@ async function handleTokenRefresh(error: { config: AxiosRequestConfig }) {
             ApiService.defaults.headers.common.Authorization = `Bearer ${data.access}`;
 
             // precisamos refazer a requisição
-            error.config.headers!.Authorization =
-                ApiService.defaults.headers.common.Authorization;
+            error.config.headers!.Authorization = `Bearer ${data.access}`;
 
             return ApiService(error.config);
-        } catch (err) {
+        } catch (error) {
             // neste caso o token-refresh não estava mais valido
+            console.log(
+                'neste caso o token-refresh não estava mais valido, error:',
+                error
+            );
             return error;
         }
     } else {
+        console.log('An empty tokenRefresh:', tokenRefresh);
         return error;
     }
 }
 
-export function linksResolver(
-    links: ApiLinksInterface[] = [],
-    name: string
-): ApiLinksInterface | undefined {
+export function linksResolver(links: ApiLinksInterface[] = [], name: string) {
     return links.find((link) => link.rel === name);
 }
 
@@ -75,8 +84,8 @@ export function ApiServiceHateoas(
 ) {
     const requestLink = linksResolver(links, name);
     if (requestLink) {
-        onCanRequest(async (data) => {
-            return await ApiService.request({
+        onCanRequest(<T>(data?: AxiosRequestConfig) => {
+            return ApiService.request<T>({
                 method: requestLink.type,
                 url: requestLink.uri,
                 ...data,
